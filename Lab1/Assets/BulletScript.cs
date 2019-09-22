2d2f5d11877abd6d43b6f52b155973ee3593ab9a
Liam McCormick <ljmc2000@gmail.com>
2019-09-22 18:26:26 +0100

    got a tank that can shoot bullets to appear

---
+++ Lab1/Assets/BulletScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    float existed4;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed=Input.GetAxis("Vertical");
+	if (speed<.1) speed=1;
+        else speed = speed*2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 10 * speed);
+        transform.Rotate(0,0,30 * Time.deltaTime);
+        existed4 += Time.deltaTime;
+
+        if(existed4 > 3f)
+        {
+	    Destroy(gameObject);
+        }
+    }
+}
