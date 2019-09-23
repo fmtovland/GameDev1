290bd4f6bc8af2a1ab6fef9c4c4aff0d8967b763
Liam Mc Cormick <ljmc2000@gmail.com>
2019-09-23 15:47:34 +0100

    added not committed files to tree

---
+++ Lab1/Assets/BrickScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color",new Vector4(Random.value,Random.value,Random.value,Random.value));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name.Equals("Bullet")){
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Lab1/Assets/WallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    public GameObject brick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                Instantiate(brick, new Vector3(i,j+.5f,3), new Quaternion());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
