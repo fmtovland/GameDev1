6856d8973701d39ae4c6ffb317ffde733b8d492e
Liam McCormick <ljmc2000@gmail.com>
2019-09-27 21:55:16 +0100

    added code to detect angle between tanks

--- Lab1/Assets/redTankScript.cs
+++ Lab1/Assets/redTankScript.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using static System.Math;
+
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class redTankScript : MonoBehaviour
 {
     public float radius;
     public int numWaypoints = 3;
-    public NavMeshAgent agent;
+    public Text console;
+    public GameObject player;
 
-    public Collider last;
+    private NavMeshAgent agent;
+    private Collider last;
     private GameObject[] targets;
     private int currentTarget = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>() as NavMeshAgent;
         targets = new GameObject[numWaypoints];
         Vector3 origin = transform.position;
         float angle = 360/numWaypoints;
 
         for(int i=0; i<numWaypoints; i++)
         {
-            targets[i]=new GameObject();
+            targets[i]=new GameObject("Waypoint "+i);
             targets[i].transform.Translate(Vector3.forward * radius);
             targets[i].transform.RotateAround(origin, Vector3.up, angle * i);
             SphereCollider sc = targets[i].AddComponent<SphereCollider>( ) as SphereCollider;
+            sc.radius = 1.5f;
             sc.isTrigger = true;
         }
     }
@@ -34,6 +41,33 @@ public class redTankScript : MonoBehaviour
     void Update()
     {
         agent.destination = targets[currentTarget].transform.position;
+        string debug = "";
+        Vector3 point = transform.InverseTransformPoint(player.transform.position);
+        double angle = Atan(point.z/point.x)*(180/PI);
+        if(point.x < 0) angle += 180;
+        if(angle < 0) angle += 360;
+
+        if(point.z > 0)
+        {
+            debug += "The player is in front. ";
+        }
+        else
+        {
+            debug += "The player is in behind. ";
+        }
+
+        debug += "Angle to player: "+angle+" ";
+
+        if( 145 > angle && angle > 45 && point.z > 0)
+        {
+            debug += "player is within FOV";
+        }
+
+        else
+        {
+            debug += "player is not within FOV";
+        }
+        console.text = debug;
     }
 
     private void OnTriggerEnter(Collider other)
