1ff8a3ad32b2a0ecf4308ff20026e78f5bdf6c32
Liam Mc Cormick <ljmc2000@gmail.com>
2019-10-11 17:41:36 +0100

    added a scene for lab4

---
+++ Lab1/Assets/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int enemyCount=5;
+    public float diameter=7;
+    public GameObject[] enemys;
+    public GameObject enemyType;
+
+    System.Collections.IEnumerator spawn() {
+        for(int i=0; true; i=(i+1)%enemyCount) {
+            if(enemys[i] == null) {
+                float x = Random.value * diameter - (diameter/2);
+                float z = Random.value * diameter - (diameter/2);
+                float angle = Random.value * 360;
+                enemys[i] = Instantiate(enemyType, new Vector3(x,5,z), new Quaternion(0,0,angle,0));
+
+                yield return new WaitForSeconds(1);
+            }
+
+            else {
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemys = new GameObject[enemyCount];
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Lab1/Assets/enemyTank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyTank : MonoBehaviour
+{
+    public int deathDelay=10;
+    public int deathEpicness=3;
+
+    System.Collections.IEnumerator ecksplode() {
+        transform.Translate(new Vector3(0,deathEpicness,0));
+        yield return new WaitForSeconds(deathDelay);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.name.Equals("Bullet")){
+            Destroy(other.gameObject);
+            StartCoroutine(ecksplode());
+        }
+    }
+}
